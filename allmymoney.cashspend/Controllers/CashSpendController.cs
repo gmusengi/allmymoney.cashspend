b18d11af9acedc706fd968e07298f1788daf8c46
Gilbert Musengi <gmusengi@gmail.com>
2019-12-23 17:08:59 +0200

    Add CashSpendController

--- allmymoney.cashspend/Controllers/CashSpendController.cs
+++ allmymoney.cashspend/Controllers/CashSpendController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,35 +12,10 @@ namespace allmymoney.cashspend.Controllers
     [ApiController]
     public class CashSpendController : ControllerBase
     {
-        // GET: api/CashSpend
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET: api/CashSpend/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
         // POST: api/CashSpend
         [HttpPost]
+        [Authorize]
         public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT: api/CashSpend/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
         {
         }
     }
